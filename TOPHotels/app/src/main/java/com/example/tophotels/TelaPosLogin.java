eadec8454aec45628a7c47a22b57daad713bc406
Francisco Jacinto Asus <2190761@my.ipleiria.pt>
2020-11-19 18:50:12 +0000

    Navigation view working

--- TOPHotels/app/src/main/java/com/example/tophotels/TelaPosLogin.java
+++ TOPHotels/app/src/main/java/com/example/tophotels/TelaPosLogin.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
@@ -24,11 +25,13 @@ public class TelaPosLogin extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_pos_login);
 
+        Toolbar tb = findViewById(R.id.toolbar);
+        setSupportActionBar(tb);
 
         nav_view = findViewById(R.id.nav_view);
         drawerLayout= findViewById(R.id.drawer_layout);
 
-        ActionBarDrawerToggle toogle = new ActionBarDrawerToggle(this, drawerLayout, R.string.ndOpen, R.string.ndClose);
+        ActionBarDrawerToggle toogle = new ActionBarDrawerToggle(this, drawerLayout, tb,R.string.ndOpen, R.string.ndClose);
 
         toogle.syncState();
         drawerLayout.addDrawerListener(toogle);
@@ -38,12 +41,18 @@ public class TelaPosLogin extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        return false;
+        Fragment frag = null;
+        if(frag != null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.contentFragment, frag).commit();
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 
     private void carregarInicio() {
         Fragment frag = null;
-        if(frag != null){
+        if (frag != null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.contentFragment, frag).commit();
         }
     }
