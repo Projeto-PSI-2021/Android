0d56f45396aa89034451e8852a46dfa2eb4c5232
João Neves <joao_rodrigo_neves@hotmail.com>
2020-12-21 17:22:03 +0000

    Forgot password, quarto selecionado e load de imagens

---
+++ TOPHotels/app/src/main/java/com/example/tophotels/vistas/EsqueceuPasswordActivity.java
@@ -0,0 +1,14 @@
+package com.example.tophotels.vistas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EsqueceuPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_esqueceu_password);
+    }
+}
